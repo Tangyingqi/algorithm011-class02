86253d8327a85fc9cfc3a517e149a3251e42bead
唐英淇 <yqtang@91souban.com>
2020-07-16 11:16:51 +0800

    Week04 homework

---
+++ Week_04/Jump.java
@@ -0,0 +1,21 @@
+
+
+public class Jump {
+
+    public int jump(int[] nums) {
+
+        int position = nums.length - 1;
+        int step = 0;
+        while (position > 0) {
+            for (int i = 0; i < position; i++) {
+                if (nums[i] + i >= position) {
+                    position = i;
+                    step++;
+                    break;
+                }
+            }
+        }
+
+        return step;
+    }
+}
